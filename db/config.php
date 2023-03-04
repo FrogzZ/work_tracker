d508495f0b9bb5c9112d73058e93aa3b14d00c79
serj <whitewatersergey@gmail.com>
2023-03-04 23:48:36 +0300

    Перевел на MYSQL с POSTGRESQL

--- db/config.php
+++ db/config.php
@@ -5,7 +5,13 @@
 //$password = 'qwerty';
 
 
+//$host='localhost';
+//$db = 'tracker';
+//$username = 'megaroot';
+//$password = '0000';
+
+
 $host='localhost';
-$db = 'tracker';
-$username = 'megaroot';
-$password = '0000';
+$db = 'mrins119_worker';
+$username = 'mrins119_moq';
+$password = 'vEgped6rPdLhdjN';
