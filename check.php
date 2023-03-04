d60ade14eac854c01a63de6f195b0aa2dfbce974
serj <whitewatersergey@gmail.com>
2023-03-04 23:29:09 +0300

    Перевел на MYSQL с POSTGRESQL

--- check.php
+++ check.php
@@ -1,10 +1,10 @@
 <?php
-//ini_set('error_reporting', E_ALL);
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
-$login = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
-$pass = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
+$login = filter_var(trim($_POST['username']), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$pass = filter_var(trim($_POST['password']), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 //echo $login . ' ' . $pass; exit();
 
@@ -20,19 +20,25 @@ if(mb_strlen($pass) < 2 || mb_strlen($pass) > 10){
 
 $pass = md5($pass . "jdpqdle");
 require "db/config.php";
-$dbconn = pg_connect("host=$host dbname=$db user=$username password=$password")
-or die('Не удалось соединиться: ' . pg_last_error());
+require "db/connect.php";
 
-$res = pg_query($dbconn, "SELECT * FROM auth WHERE login = '$login' AND pass = '$pass'");
-
-$get_user = pg_fetch_assoc($res);
-pg_close($dbconn);
-
-if($get_user === false){
+$sql = "SELECT * FROM auth WHERE login = '$login' AND pass = '$pass'";
+$result = mysqli_query($conn, $sql);
+$get_user = mysqli_fetch_assoc($result);
+$conn->close();
+//$dbconn = pg_connect("host=$host dbname=$db user=$username password=$password")
+//or die('Не удалось соединиться: ' . pg_last_error());
+//
+//$res = pg_query($dbconn, "SELECT * FROM auth WHERE login = '$login' AND pass = '$pass'");
+//
+//$get_user = pg_fetch_assoc($res);
+//pg_close($dbconn);
+//
+if($get_user === NULL){
     echo "Пользователь не найден";
     exit();
 }
 
 setcookie('user', $get_user['login'], time() + 3600 * 24 * 30, "/");
 
-header('/');
+header('Location: /');
